80852a469eb1498ec54b70e8efca0cbba8833d5c
Tony Gustafsson <mail@tonyg.se>
2017-07-17 15:22:25 +0200

    Absolute path for client build

--- client/client.js
+++ client/client.js
@@ -4,7 +4,7 @@ const express = require('express'),
 	app = express(),
 	fs = require('fs');
 
-app.use(express.static('build'));
+app.use(express.static('/home/tony.gustafsson/lunchdate/client/build'));
 
 app.listen(80, function () {
 	console.log('Starting web server on http://localhost:80/');
