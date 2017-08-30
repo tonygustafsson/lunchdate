6854aeafb68e8f252aa178d031b81c652f0d3f3e
Tony Gustafsson <mail@tonyg.se>
2017-08-30 21:12:56 +0200

    Add warning if you are anonymous

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Button, Icon } from 'react-mdl';
 
-const Header = ({ showNewDateForm, datesShowNewDateForm }) => {
+const Header = ({ showNewDateForm, datesShowNewDateForm, name, userToggleEditMode }) => {
   return (
     <div>
-      <h1  onClick={() => { datesShowNewDateForm(false) }}>Lunch date</h1>
+      <h1 onClick={() => { datesShowNewDateForm(false) }}>Lunch date</h1>
 
       {!showNewDateForm &&
         <Button raised accent ripple className="create-date-button" onClick={() => { datesShowNewDateForm(!showNewDateForm) }}>
           <Icon name="add" /> Create new date
         </Button>
+      }
+
+      { name.toLowerCase() === 'anonymous' &&
+        <div className="anonymous-warning" onClick={() => {userToggleEditMode() }}>
+          <p>
+            <Icon name="warning" />
+            You haven't set your name yet, so you will be known as Anonymous.
+            Click here to set your name.
+          </p>
+        </div>
       }
     </div>
   );
--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
-import { datesShowNewDateForm } from '../actions';
+import { datesShowNewDateForm, userToggleEditMode } from '../actions';
 import Header from '../Components/Header/Header';
 import './Header.css';
 
 const mapStateToProps = (state, ownProps) => {
   return {
     showNewDateForm: state.dates.showNewDateForm,
+    name: state.user.name,
   };
 };
 
@@ -13,7 +14,8 @@ export const HeaderComponent = connect(
   mapStateToProps,
   (dispatch) => {
     return {
-      datesShowNewDateForm: (showNewDateForm) => { dispatch(datesShowNewDateForm(showNewDateForm)); },      
+      datesShowNewDateForm: (showNewDateForm) => { dispatch(datesShowNewDateForm(showNewDateForm)); },
+      userToggleEditMode: () => { dispatch(userToggleEditMode()); }
     };
   }
 )(Header);
