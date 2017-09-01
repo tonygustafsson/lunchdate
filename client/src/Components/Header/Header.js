6ed27fc9f3797faaa91a716068d6ca673220f4b5
Tony Gustafsson <mail@tonyg.se>
2017-09-01 18:41:40 +0200

    Header as  a class component

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Button, Icon } from 'react-mdl';
 
-const Header = ({ showNewDateForm, datesShowNewDateForm, name, userToggleEditMode }) => {
-  return (
-    <div>
-      <h1 onClick={() => { datesShowNewDateForm(false) }}>Lunch date</h1>
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-      {!showNewDateForm &&
-        <Button raised accent ripple className="create-date-button" onClick={() => { datesShowNewDateForm(!showNewDateForm) }}>
-          <Icon name="add" /> Create new date
+  render() {
+    return (
+      <div>
+        <h1 onClick={() => { this.props.datesShowNewDateForm(false) }}>Lunch date</h1>
+
+        {!this.props.showNewDateForm &&
+          <Button raised accent ripple className="create-date-button" onClick={() => { this.props.datesShowNewDateForm(!this.props.showNewDateForm) }}>
+            <Icon name="add" /> Create new date
         </Button>
-      }
+        }
 
-      { name.toLowerCase() === 'anonymous' &&
-        <div className="anonymous-warning" onClick={() => {userToggleEditMode() }}>
-          <p>
-            <Icon name="warning" />
-            You haven't set your name yet, so you will be known as Anonymous.
-            Click here to set your name.
+        {this.props.name.toLowerCase() === 'anonymous' &&
+          <div className="anonymous-warning" onClick={() => { this.props.userToggleEditMode() }}>
+            <p>
+              <Icon name="warning" />
+              You haven't set your name yet, so you will be known as Anonymous.
+              Click here to set your name.
           </p>
-        </div>
-      }
-    </div>
-  );
+          </div>
+        }
+      </div>
+    );
+  }
 }
-
-export default Header;
